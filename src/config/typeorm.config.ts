ee0b5969689419e44af00d02c49c77cbf728acbc
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-11-22 22:14:33 +0330

    change databse to postgres and comment out unused functions

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,43 +1,43 @@
-import {TypeOrmModuleOptions} from "@nestjs/typeorm";
-import {config} from "dotenv";
-import {join} from "path";
-import {DataSourceOptions} from "typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { join } from 'path';
+import { DataSourceOptions } from 'typeorm';
 
 config();
 config({
   path: join(process.cwd(), `.env.${process.env.NodeEnv}`),
 });
 export function TypeOrmConfig(): TypeOrmModuleOptions {
-  const {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME} = process.env;
+  const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } = process.env;
   return {
-    type: "mysql",
+    type: 'postgres',
     host: DB_HOST,
-    port: DB_PORT,
+    port: DB_PORT, // parseInt(DB_PORT, 10),
     username: DB_USERNAME,
-    password: DB_PASSWORD,
+    password: DB_PASSWORD || null,
     database: DB_NAME,
     autoLoadEntities: false,
     synchronize: true,
     entities: [
-      "dist/**/**/**/*.entity{.ts,.js}",
-      "dist/**/**/*.entity{.ts,.js}",
+      'dist/**/**/**/*.entity{.ts,.js}',
+      'dist/**/**/*.entity{.ts,.js}',
     ],
   };
 }
 
-export function TypeOrmDataSourceConfig(): DataSourceOptions {
-  const {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME} = process.env;
-  return {
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "snappfood",
-    synchronize: true,
-    entities: [
-      "dist/**/**/**/*.entity{.ts,.js}",
-      "dist/**/**/*.entity{.ts,.js}",
-    ],
-  };
-}
+// export function TypeOrmDataSourceConfig(): DataSourceOptions {
+//   const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } = process.env;
+//   return {
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: '',
+//     database: 'snappfood',
+//     synchronize: true,
+//     entities: [
+//       'dist/**/**/**/*.entity{.ts,.js}',
+//       'dist/**/**/*.entity{.ts,.js}',
+//     ],
+//   };
+// }
