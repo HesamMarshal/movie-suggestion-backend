6c6d51bb198090ce0e331af856d100350cc32ecb
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-11-22 18:49:24 +0330

    add more core things to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfigInit } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configure Swagger
+  SwaggerConfigInit(app);
+
+  // Static folder
+  app.useStaticAssets('public');
+
+  // Enable global validation pipe
+  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip unvalidated properties
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Activate Cookie Parser
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+
+  // Start Server
+  const PORT = process.env.PORT;
+  console.log(process.env);
+
+  await app.listen(PORT, () => {
+    console.log(`Root:    http://localhost:${PORT}`);
+    console.log(`Swagger: http://localhost:${PORT}/swagger`);
+  });
 }
 bootstrap();
